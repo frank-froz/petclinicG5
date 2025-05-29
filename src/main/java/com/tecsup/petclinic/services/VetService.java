d94b27992577702f160e86d40dfb8c37b6ebf4d8
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-29 16:53:05 -0500

    Agrega el test de creación de veterinarios

--- src/main/java/com/tecsup/petclinic/services/VetService.java
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -1,4 +1,9 @@
 package com.tecsup.petclinic.services;
 
+import java.util.List;
+import com.tecsup.petclinic.entities.Vet;
+
 public interface VetService {
+
+    Vet create(Vet vet);
 }
--- src/main/java/com/tecsup/petclinic/services/VetServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/VetServiceImpl.java
@@ -1,4 +1,20 @@
 package com.tecsup.petclinic.services;
 
-public class VetServiceImpl {
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.repositories.VetRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class VetServiceImpl implements VetService {
+
+    VetRepository vetRepository;
+
+    public VetServiceImpl(VetRepository vetRepository) {this.vetRepository = vetRepository; }
+
+    @Override
+    public Vet create(Vet vet){
+        return vetRepository.save(vet);
+    }
 }
