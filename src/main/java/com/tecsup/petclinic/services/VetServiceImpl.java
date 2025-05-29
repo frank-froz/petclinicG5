3179ebc1d19e67ee5833ce6ea2dcfeaa565e7aca
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-05-29 16:56:44 -0500

    Se subió cambios de busqueda en VetServiceImpl

--- src/main/java/com/tecsup/petclinic/services/VetServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/VetServiceImpl.java
@@ -1,4 +1,96 @@
 package com.tecsup.petclinic.services;
 
-public class VetServiceImpl {
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.VetNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+public class VetServiceImpl implements VetService {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public Vet findById(Integer id) throws VetNotFoundException {
+
+        Optional<Vet> vet = vetRepository.findById(id);
+
+        if ( !vet.isPresent())
+            throw new VetNotFoundException("Record not found...!");
+
+        return vet.get();
+    }
+    @Override
+    public List<Vet> findByName(String name) {
+
+        List<Vet> vets = vetRepository.findByName(name);
+
+        vets.stream().forEach(vet -> log.info("" + vet));
+
+        return vets;
+    }
+
+    @Override
+    public List<Vet> findAll() {
+        //
+        return VetRepository.findAll();
+
+    }
 }
