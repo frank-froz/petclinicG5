e6941c31c0517b0af9f93ca3ea8c2da19a77ed1a
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-05-29 16:56:03 -0500

    Se subió cambios de busqueda en VetRespository

--- src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
@@ -5,7 +5,14 @@ import com.tecsup.petclinic.entities.Vet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VetRepository
         extends JpaRepository<Vet, Integer> {
+
+        List<Vet> findByName(String name);
+
+        @Override
+        List<Vet> findAll();
 }
