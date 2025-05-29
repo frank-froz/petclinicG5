2efa2170a06cf90ef7127e11a59baee037336581
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-29 16:27:01 -0500

    Modifica el vetRepository para el uso de extends

--- src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
@@ -1,4 +1,11 @@
 package com.tecsup.petclinic.repositories;
 
-public interface VetRepository {
+
+import com.tecsup.petclinic.entities.Vet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VetRepository
+        extends JpaRepository<Vet, Integer> {
 }
