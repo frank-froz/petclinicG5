be42c4a0cf325a95fbeb2d8c984466ce758d6e39
Han-28 <hector.castro@tecsup.edu.pe>
2025-05-29 17:33:51 -0500

    Agrega: test update

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -34,6 +34,37 @@ public class VetServiceTest {
         assertEquals(VET_LAST_NAME, vetCreated.getLast_name());
 
     }
+    @Test
+    public void testUpdateVet() {
 
-	
+        String FIRST_NAME = "Castro";
+        String LAST_NAME = "Peñaloza";
+
+        String UP_FIRST_NAME = "Hanmer";
+        String UP_LAST_NAME = "Hector";
+        Vet vet = new Vet(FIRST_NAME, LAST_NAME);
+
+        // ------------ Create ---------------
+
+        log.info(">" + vet);
+        Vet vetCreated = this.vetService.create(vet);
+        log.info(">>" + vetCreated);
+
+        // ------------ Update ---------------
+
+        // Prepare data for update
+        vetCreated.setFirst_name(UP_FIRST_NAME);
+        vetCreated.setLast_name(UP_LAST_NAME);
+
+        // Execute update
+        Vet upgradeVet = this.vetService.update(vetCreated);
+        log.info(">>>>" + upgradeVet);
+
+        //            EXPECTED        ACTUAL
+        assertEquals(UP_FIRST_NAME, upgradeVet.getFirst_name());
+        assertEquals(UP_LAST_NAME, upgradeVet.getLast_name());
+
+    }
+
+
 }
