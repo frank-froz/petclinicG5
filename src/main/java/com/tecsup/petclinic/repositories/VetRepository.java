e67c602a1a3a79adc06457d72179896ce0fcad85
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-29 16:52:38 -0500

    Modifica el vetRepository

--- src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
@@ -5,7 +5,13 @@ import com.tecsup.petclinic.entities.Vet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VetRepository
         extends JpaRepository<Vet, Integer> {
+
+    @Override
+    List<Vet> findAll();
+
 }
