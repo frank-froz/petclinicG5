eef4e34b69dca58ff257e1bc0194d8765e437888
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-05-29 23:28:13 -0500

    se modifico VetService

--- src/main/java/com/tecsup/petclinic/services/VetService.java
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -6,4 +6,7 @@ import com.tecsup.petclinic.entities.Vet;
 public interface VetService {
 
     Vet create(Vet vet);
+    Vet findById(Integer id);
+    List<Vet> findByName(String name);
+    List<Vet> findAll();
 }
